237994349c33ae71c24aad4a33b80e600786e9b5
Jose Annunziato <jannunzi@gmail.com>
2018-02-23 10:46:30 -0500

    first commit

---
+++ src/main/java/edu/neu/cs4500/SpringBootWebApplication.java
@@ -0,0 +1,12 @@
+package edu.neu.cs4500;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebApplication.class, args);
+	}
+}
